4fde41995db5242ff9017c07d596fd7a616d9596
Nathan Farrel <nathanfarrel1128@gmail.com>
2023-05-11 15:19:37 +0700

    second commit

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ mongoose.Promise = global.Promise;
 main().catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb+srv://admin:admin@cluster0.weaokyc.mongodb.net/BlogDB");
+  await mongoose.connect(process.env.URL);
 }
 
 const blogSchema = new mongoose.Schema({
